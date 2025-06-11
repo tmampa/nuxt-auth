a30480cfec2f6eb63128c83a65e69885b2f78c35
tmampa <tmampa94@gmail.com>
2025-06-11 16:30:48 +0200

    Remove unused Tailwind CSS configuration and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/tailwindcss"],
   nitro: {
     prerender: {
       routes: ["/"],
     },
-  },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ]
   },
   app: {
     baseURL: process.env.NODE_ENV === "production" ? "/auth/" : "/",
