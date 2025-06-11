e723d04157b9cafeeb80e5117fe8bc86781e8abd
tmampa <tmampa94@gmail.com>
2025-06-11 17:33:27 +0200

    Update welcome message in app.vue to clarify project purpose

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
     <h1 class="font-bold text-center text-4xl text-pink-600">Tailwind + Nuxt Auth</h1>
-    <p>Welcome to Next Auth</p>
+    <p>This is a Nuxt Authentication Project</p>
   </div>
 </template>
