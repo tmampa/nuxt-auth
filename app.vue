0428636096b3fe0dd631af5652baea7211bf2a43
tmampa <tmampa94@gmail.com>
2025-06-11 17:50:23 +0200

    Add Tailwind CSS and DaisyUI integration; update app.vue and nuxt.config.ts

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
     <h1 class="font-bold text-center text-4xl text-pink-600">Tailwind + Nuxt Auth</h1>
     <p>This is a Nuxt Authentication Project</p>
+    <button class="btn btn-primary">Primary</button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/tailwindcss"],
-  
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["./assets/style.css"],
 });
