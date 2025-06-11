19a0744174c51fbef41cdb1b4f9f439110c72d78
tmampa <tmampa94@gmail.com>
2025-06-11 16:42:09 +0200

    Enhance app.vue layout with Tailwind CSS for improved styling and responsiveness

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Next Auth</h1>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="font-bold text-center text-4xl text-pink-600">Next Auth</h1>
     <p>Welcome to Next Auth</p>
   </div>
 </template>
