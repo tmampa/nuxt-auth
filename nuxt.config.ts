3492515dc51a8058ba7c479cce233e8b50e27e85
tmampa <tmampa94@gmail.com>
2025-06-12 19:31:21 +0200

    Implement code changes to enhance functionality and improve performance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/tailwindcss"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/tailwindcss", "nuxt-mongoose"],
   css: ["./assets/style.css"],
+  mongoose: {
+    uri: process.env.MONGODB_URI ,
+  }
 });
