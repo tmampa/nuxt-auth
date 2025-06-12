8c1d0767dde4b7bcc9684d4ef33736aad5bbd6d0
tmampa <tmampa94@gmail.com>
2025-06-12 17:53:21 +0200

    Implement initial layout and authentication pages; add default layout, login, register, and index pages

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <h1 class="font-bold text-center text-4xl text-pink-600">Tailwind + Nuxt Auth</h1>
-    <p>This is a Nuxt Authentication Project</p>
-    <button class="btn btn-primary">Primary</button>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto p-4 flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-3xl font-bold mb-4">Nuxt + <span class="text-secondary">Better Auth</span></h1>
+    <p class="text-lg mb-8">Welcome to the Nuxt application with Better Auth integration!</p>
+    <div class="flex space-x-4">
+        <NuxtLink to="auth/login" class="btn btn-primary">Login</NuxtLink>
+        <NuxtLink to="auth/register" class="btn btn-accent btn-outline">Sign Up</NuxtLink>
+    </div>
+    
+  </div>
+</template>
